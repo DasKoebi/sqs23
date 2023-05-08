05b3d59e3fd69fe3dbbcc855972ee7050ee02180
Lukas Kolb <luk4games@gmail.com>
2023-05-08 11:18:26 +0200

    (fet) API-Key as env

--- src/main/java/inf/isoToName/proxy/GoogleISOApi.java
+++ src/main/java/inf/isoToName/proxy/GoogleISOApi.java
@@ -3,9 +3,9 @@ package inf.isoToName.proxy;
 import java.io.IOException;
 
 public class GoogleISOApi {
-    private static String baseUri="https://www.googleapis.com/books/v1/volumes?q=isbn:";
+    private static final String apiKey = System.getenv().get("API_KEY");
+    private static final String baseUri = System.getenv().getOrDefault("BASE_URI", "https://www.googleapis.com/books/v1/volumes?q=isbn:");
     //order getProperty("");
-    private String apiKey = "";
     public String getName(String isbn) throws IOException, InterruptedException {
         System.out.println(apiKey);
         String url = baseUri+isbn+"&key="+apiKey;
