e6cf7ebda32b4ec07dbb28a099ea3188d4b23738
Lukas Kolb <luk4games@gmail.com>
2023-05-15 14:50:09 +0200

    (feat) added Tests for BookService

--- src/main/java/inf/iso_to_name/books/BookService.java
+++ src/main/java/inf/iso_to_name/books/BookService.java
@@ -17,7 +17,7 @@ public class BookService {
     private static final Logger logger = Logger.getLogger(BookService.class.getName());
 
     //@Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
     @Autowired
     public BookService(BookRepository bookRepository){
@@ -28,11 +28,11 @@ public class BookService {
         try {
             int length = isbn.length();
             //check if ISBN is 10 or 13 digits
-            if (length == 10 || length == 13) {
+            if (length == 10 && is_isbn10_valid(isbn)|| length == 13 && is_isbn13_valid(isbn)) {
 
             Double.parseDouble(isbn);
-            //check if ISBN is stored in Database
 
+            //check if ISBN is stored in Database
             List<Book> myBook = bookRepository.findByisbn10(isbn);
 
             if(myBook.isEmpty()){
@@ -45,7 +45,7 @@ public class BookService {
                 }return myBook.get(0).getName();
             }return myBook.get(0).getName();
             }
-            return "The ISBN must have 10 or 13 digits.";
+            return "Not a valid ISBN";
 
         } catch (IOException e) {
             logger.log(Level.SEVERE, e.getMessage(), e);
@@ -86,4 +86,34 @@ public class BookService {
     private void saveBookinDatabase(Book book){
         bookRepository.save(book);
     }
+
+    public boolean is_isbn10_valid(String isbn){
+        char[] tmp = isbn.toCharArray();
+        int a =0;
+
+        for (int i = 1; i<10; i++){
+            a +=Integer.parseInt(String.valueOf(tmp[i-1]))*i;
+        }
+        int rest = a % 11;
+        if(rest == 10) return (tmp[9]=="X".toCharArray()[0] || tmp[9]=="x".toCharArray()[0]);
+        return rest == Integer.parseInt(String.valueOf(tmp[9]));
+    }
+
+    public boolean is_isbn13_valid(String isbn){
+        char[] tmp = isbn.toCharArray();
+        int a ;
+        int b= 0;
+        for (int i = 1; i<13; i++){
+            a = Integer.parseInt(String.valueOf(tmp[i-1]));
+            if(i%2 == 0) {
+                b += a*3;
+            } else {
+                b += a;
+            }
+        }
+        int rest = b % 10;
+        return tmp[12] == 0 || (Integer.parseInt(String.valueOf(tmp[12])) + rest) == 10;
+    }
+
+
 }
---
+++ src/test/java/inf/iso_to_name/books/BookServiceTest.java
@@ -0,0 +1,75 @@
+package inf.iso_to_name.books;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class BookServiceTest {
+
+
+    private BookService bookService;
+    @BeforeEach
+    public void setUp(){
+        Book tmp = new Book();
+        tmp.setIsbn10("1569319014");
+        tmp.setIsbn13("9781569319017");
+        tmp.setName("One Piece, Vol. 1");
+        tmp.setId(1);
+        List<Book> myBook = new ArrayList<>();
+        myBook.add(tmp);
+        BookRepository bookRepository = Mockito.mock(BookRepository.class);
+        when(bookRepository.findByisbn10("1569319014")).thenReturn(myBook);
+        when(bookRepository.findByisbn13("9781569319017")).thenReturn(myBook);
+        bookService = new BookService(bookRepository);
+    }
+
+    @Test
+    void testGetBookByIsbn10() {
+        try {
+            assertEquals("One Piece, Vol. 1", bookService.getBookByIsbn("1569319014"));
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testValidIsbn(){
+        assertTrue(bookService.is_isbn10_valid("1569319014"));
+    }
+    @Test
+    void testValidIsbn2(){
+        assertTrue(bookService.is_isbn10_valid("3644009473"));
+    }
+    @Test
+    void testValidIsbnWithX(){
+        assertTrue(bookService.is_isbn10_valid("354059101X"));
+    }
+    @Test
+    void testValidIsbnWithSmallX(){
+        assertTrue(bookService.is_isbn10_valid("354059101x"));
+    }
+
+    @Test
+    void testInvalidIsnb(){
+        assertFalse(bookService.is_isbn10_valid("1234567890"));
+    }
+
+    @Test
+    void testValidIsbn13(){
+        assertTrue(bookService.is_isbn13_valid("9781569319017"));
+    }
+
+    @Test
+    void testInvalidIsbn13(){
+        assertFalse(bookService.is_isbn13_valid("9781569318017"));
+    }
+
+}
