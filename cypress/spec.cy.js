687ea434fa87afba83a17efe06715266173aa87b
Lukas Kolb <luk4games@gmail.com>
2023-06-24 11:53:25 +0200

    feat(cypress): added e2e test

---
+++ cypress/spec.cy.js
@@ -0,0 +1,17 @@
+describe('template spec', () => {
+    it('passes', () => {
+        //open homepage    
+        cy.visit('http://localhost:8080/');
+    
+        //Find text field and insert 1569319014  
+        cy.get('#isbnInput').type('1569319014');
+        //submit
+        cy.get('form').submit();
+    
+        //check new url
+        cy.url().should('include', '/v1/books/1569319014');
+    
+        //check if the title is visible
+        cy.contains('One Piece, Vol. 1').should('be.visible');
+    })
+  })
